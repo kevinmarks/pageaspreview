9ca2d3be0b6223bc40b2586750fd740955cc8dfd
Kevin Marks <kevinmarks@gmail.com>
2013-02-22 12:41:17 -0800

    name it properly

--- web.js
+++ web.js
@@ -6,10 +6,6 @@ app.configure(function(){
   app.use(express.static(__dirname + '/web'));
 });
 
-app.get('/fix', function(request, response) {
-  response.send('Fix Patents!');
-});
-
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
